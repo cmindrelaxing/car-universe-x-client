9c1b521faad9dd20de848915ae7146c53589fd75
prographicsten <prographicsten@gmail.com>
2023-10-20 17:43:59 +0600

    AOS add

--- src/components/UpdateProduct/UpdateProduct.jsx
+++ src/components/UpdateProduct/UpdateProduct.jsx
@@ -44,10 +44,10 @@ const UpdateProduct = () => {
 
     return (
         <div className="bg-[#F4F3F0] px-4 md:p-10 lg:p-24 py-10">
-            <h2 className=" text-base md:text-3xl font-extrabold mb-5">Update Car Information : {name}</h2>
+            <h2 className=" text-base md:text-3xl font-extrabold mb-5" data-aos="fade-up">Update Car Information : {name}</h2>
             <form onSubmit={handleUpdate}>
                 {/* form Car Name and Category row */}
-                <div className="md:flex gap-5 mb-8">
+                <div className="md:flex gap-5 mb-8" data-aos="fade-up">
                     <div className="form-control md:w-1/2 mb-4 md:mb-0">
                         <label className="label">
                             <span className="label-text">Car Name</span>
@@ -66,7 +66,7 @@ const UpdateProduct = () => {
                     </div>
                 </div>
                 {/* form Price and Description row */}
-                <div className="md:flex gap-5 mb-8">
+                <div className="md:flex gap-5 mb-8" data-aos="fade-up">
                     <div className="form-control md:w-1/2 mb-4 md:mb-0">
                         <label className="label">
                             <span className="label-text">Price</span>
@@ -85,7 +85,7 @@ const UpdateProduct = () => {
                     </div>
                 </div>
                 {/* form Rating and image URL row */}
-                <div className="md:flex gap-5 mb-8">
+                <div className="md:flex gap-5 mb-8" data-aos="fade-up">
                     <div className="form-control md:w-1/2 mb-4 md:mb-0">
                         <label className="label">
                             <span className="label-text">Rating</span>
